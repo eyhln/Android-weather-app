7531f2a95c8547784f613fae3d01e06300084024
Marie Byleen <mbyleen@gmail.com>
2016-11-15 12:36:05 -0600

    Test if google play services are available

--- app/src/main/java/com/mariebyleen/weather/LocationFragment.java
+++ app/src/main/java/com/mariebyleen/weather/LocationFragment.java
@@ -3,18 +3,52 @@ package com.mariebyleen.weather;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 public class LocationFragment extends Fragment {
+
+    private LocationContract.Presenter presenter;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_location, container, false);
+        View view = inflater.inflate(R.layout.fragment_location, container, false);
+        ButterKnife.bind(this, view);
+        return view;
     }
+
+    @OnClick(R.id.button_use_current_location)
+    public void useCurrentLocation() {
+        if (playServicesAvailableOnDevice()) {
+
+        }
+    }
+
+
+    private boolean playServicesAvailableOnDevice() {
+        GoogleApiAvailability availability = GoogleApiAvailability.getInstance();
+        int status = availability.isGooglePlayServicesAvailable(getContext());
+        if(status == ConnectionResult.SUCCESS)
+            return true;
+        else {
+            Log.d("STATUS", "Google Play Services not available on this device. Code: "
+                    + String.valueOf(status));
+            return false;
+        }
+    }
+
+
+
 
 
 
