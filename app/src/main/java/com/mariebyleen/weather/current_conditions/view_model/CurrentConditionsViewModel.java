1b06ed315b5a2f848e01b0e55543424a28dc001f
Marie Byleen <mbyleen@gmail.com>
2016-11-25 13:28:03 -0600

    Simplify view
    remove all redudancy while troubleshooting issues with data binding

--- app/src/main/java/com/mariebyleen/weather/current_conditions/view_model/CurrentConditionsViewModel.java
+++ app/src/main/java/com/mariebyleen/weather/current_conditions/view_model/CurrentConditionsViewModel.java
@@ -1,8 +1,11 @@
 package com.mariebyleen.weather.current_conditions.view_model;
 
+import android.content.SharedPreferences;
 import android.databinding.BaseObservable;
+import android.databinding.Bindable;
 import android.util.Log;
 
+import com.google.gson.Gson;
 import com.mariebyleen.weather.api.OpenWeatherApiService;
 import com.mariebyleen.weather.current_conditions.model.CurrentConditionsResponse;
 
@@ -13,6 +16,8 @@ import rx.Observer;
 import rx.android.schedulers.AndroidSchedulers;
 import rx.schedulers.Schedulers;
 
+import static com.mariebyleen.weather.application.WeatherApplication.getApiKey;
+
 public class CurrentConditionsViewModel extends BaseObservable
         implements Observer<CurrentConditionsResponse> {
 
@@ -21,17 +26,32 @@ public class CurrentConditionsViewModel extends BaseObservable
     private double lon = 0;
 
     private OpenWeatherApiService weatherApiService;
+    private Gson gson;
+    private SharedPreferences preferences;
     private CurrentConditionsResponse response;
 
     @Inject
-    public CurrentConditionsViewModel(OpenWeatherApiService weatherApiService) {
+    public CurrentConditionsViewModel(OpenWeatherApiService weatherApiService, Gson gson,
+                                      SharedPreferences preferences) {
         this.weatherApiService = weatherApiService;
+        this.gson = gson;
+        this.preferences = preferences;
+        if (response == null) {
+            populateData();
+        }
         refreshWeatherData();
+    }
+
+    private void populateData() {
+        String json = preferences.getString("CurrentConditions", "");
+        CurrentConditionsResponse conditions = gson.fromJson(json,
+                CurrentConditionsResponse.class);
+        response = conditions;
     }
 
     private void refreshWeatherData() {
         Observable<CurrentConditionsResponse> response =
-                weatherApiService.getCurrentConditions(lat, lon);
+                weatherApiService.getCurrentConditions(getApiKey());
         response
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
@@ -46,26 +66,25 @@ public class CurrentConditionsViewModel extends BaseObservable
     @Override
     public void onError(Throwable e) {
         Log.e(TAG, "Error retrieving current conditions weather data");
+        e.printStackTrace();
     }
 
     @Override
     public void onNext(CurrentConditionsResponse currentConditionsResponse) {
         response = currentConditionsResponse;
+        Log.d(TAG, "Temperature: " + currentConditionsResponse.getMain().getTemp());
+        Log.d(TAG, "Location: " + currentConditionsResponse.getName());
         notifyChange();
     }
 
+    @Bindable
     public String getTemperature() {
-        double temperature = response.getMain().getTemp();
-        return String.valueOf(temperature);
-    }
-
-    public String getPressure() {
-        double pressure = response.getMain().getPressure();
-        return String.valueOf(pressure);
-    }
-
-    public String getHumidity() {
-        double humidity = response.getMain().getHumidity();
-        return String.valueOf(humidity);
+        if (response != null) {
+            double temperature = response.getMain().getTemp();
+            Log.d(TAG, "getTemperature method called");
+            return "Temperature: " + String.valueOf(temperature);
+        }
+        else
+            return "";
     }
 }
