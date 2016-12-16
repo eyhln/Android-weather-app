ce4139e03ff33aff6d8a7ff4461465b83498494e
Marie Byleen <mbyleen@gmail.com>
2016-12-16 11:26:27 -0600

    withdraw update job scheduling from fragment lifecycle to application level
    avoid rescheduling jobs for every configuration change, instead listen for changes in the update period preference.

--- app/src/main/java/com/mariebyleen/weather/application/di/component/ApplicationComponent.java
+++ app/src/main/java/com/mariebyleen/weather/application/di/component/ApplicationComponent.java
@@ -7,8 +7,9 @@ import android.content.res.Resources;
 import com.evernote.android.job.JobManager;
 import com.google.gson.Gson;
 import com.mariebyleen.weather.application.di.module.ApplicationModule;
-import com.mariebyleen.weather.job.WeatherJobCreator;
+import com.mariebyleen.weather.weather_display.job.WeatherJobCreator;
 import com.mariebyleen.weather.navigation.Navigator;
+import com.mariebyleen.weather.weather_display.activity.MainActivity;
 import com.mariebyleen.weather.weather_display.current_conditions.view.CurrentConditionsFragment;
 
 import javax.inject.Singleton;
@@ -28,5 +29,6 @@ public interface ApplicationComponent {
 
     void inject(WeatherJobCreator weatherJobCreator);
     void inject(CurrentConditionsFragment currentConditionsFragment);
+    void inject(MainActivity mainActivity);
 
 }
